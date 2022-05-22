0d120b128c00b94523d86c8c055ea7bf326b2b81
Sriyanjith Herath <64471315+sriyabro@users.noreply.github.com>
2022-05-22 23:42:16 +0530

    Feature/top bar (#3)
    * bootstrap & sass updated
    
    * font added
    
    * top bar completed
    
    * top-bar button hover changes

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -7,7 +7,7 @@ const TopBar = () => {
         <Navbar collapseOnSelect expand="lg" className="w-100 p-0 d-flex justify-content-end">
           <div className="top-bar-item">
             <Phone size={15} className="icon" />
-            <span>+94 77 123 45 67</span>
+            <span><a href="tel:+94771234567">+94 77 123 45 67</a></span>
           </div>
           <Navbar.Toggle />
           <Navbar.Collapse className="flex-grow-0">
@@ -23,7 +23,7 @@ const TopBar = () => {
               <span className="top-bar-btn border-btn px-3 py-1">Register</span>
             </div>
             <div className="top-bar-item">
-              <span className="top-bar-btn">Login</span>
+              <span className="top-bar-btn px-3 py-1">Login</span>
             </div>
           </Navbar.Collapse>
         </Navbar>
