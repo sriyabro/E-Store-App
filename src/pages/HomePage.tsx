a32a4c57d34336c9b022a81bad6c043c8bf482cc
sriyabro <sriyabro@gmail.com>
2022-05-22 18:27:17 +0530

    logo nav bar done

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,17 @@
 import { Container, Row } from "react-bootstrap";
+import LogoNavBar from "../components/LogoNavBar";
 import TopBar from "../components/TopBar";
 
 const HomePage = () => {
   return (
     <Container fluid={true}>
-        <Row>
-            <TopBar/>
-        </Row>
+      <Row>
+        <TopBar />
+      </Row>
+      <Row>
+        <LogoNavBar />
+      </Row>
     </Container>
-  )
-}
+  );
+};
 export default HomePage;
